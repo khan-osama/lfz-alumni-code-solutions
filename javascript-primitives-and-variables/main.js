6703bb91c014fbb3efb97bf84b1ef4aad58445f3
khan-osama <osamaa.khan61@gmail.com>
2022-03-16 22:37:27 +0000

    Created variables and primitives and logged to console

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Osama Khan';
+var isCool = true;
+var totalPets = 2;
+var unknown = null;
+var never;
+
+console.log('Full name: ', fullName);
+console.log('Is Osama Khan cool? ', isCool);
+console.log('How many pets does he have? ', totalPets);
+
+console.log('Typeof for name: ', typeof(fullName));
+console.log('Typeof for cool: ', typeof(isCool));
+console.log('Typeof for pets: ', typeof(totalPets));
+
+console.log('value of unknown: ', unknown);
+console.log('Typeof for unknown: ', typeof(unknown));
+
+console.log('value of never: ', never);
+console.log('Typeof for never: ', typeof(never));
