1cbfa564f542d58b12d659e8b62177b7ee449b6d
khan-osama <osamaa.khan61@gmail.com>
2022-03-17 01:11:21 +0000

    Completed assignment and logged values into console

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+var width = 5;
+var height = 10;
+var area = width * height;
+
+console.log("Area: ", area);
+console.log("Typeof Area: ", typeof(area));
+
+var bill = 25.50;
+var payment = 30.00;
+var change = payment - bill;
+
+console.log("Change: ", change);
+console.log("Typeof Change: ", typeof(change));
+
+var quiz = 85;
+var midterm = 76;
+var final = 90;
+var grade = (quiz + midterm + final) / 3;
+
+console.log("Grade: ", grade);
+console.log("Typeof Grade: ", typeof(grade));
+
+var firstName = "Osama";
+var lastName = "Khan";
+var fullName = firstName + " " + lastName;
+
+console.log("Full name: ", fullName);
+console.log("Typeof Full name: ", typeof(fullName));
+
+var pH = 5.5;
+var isAcidic = pH < 7;
+
+console.log("Is Acidic: ", isAcidic);
+console.log("Typeof isAcidic: ", typeof(isAcidic));
+
+var headCount = 234;
+var isSparta = (headCount === 300);
+
+console.log("isSparta: ", isSparta);
+console.log("Typeof isSparta: ", typeof(isSparta));
+
+var motto = fullName;
+motto = motto + ' ' + 'is the GOAT';
+
+console.log("Motto: ", motto);
+console.log("Typeof Motto: ", typeof(motto));
