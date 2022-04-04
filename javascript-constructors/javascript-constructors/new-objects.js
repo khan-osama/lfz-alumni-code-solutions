fca5d41a1ee1c15bf09711c9f7aa7a5a06961590
khan-osama <osamaa.khan61@gmail.com>
2022-04-04 12:35:38 -0700

    Created student constructor function to pass tests

---
+++ javascript-constructors/javascript-constructors/new-objects.js
@@ -0,0 +1,20 @@
+var anObject = new Object();
+var anArray = new Array();
+var aFunction = new Function();
+var aString = new String();
+var aNumber = new Number();
+var aBoolean = new Boolean();
+
+console.log('value of anObject: ', anObject);
+console.log('value of anArray: ', anArray);
+console.log('value of aFunction: ', aFunction);
+console.log('value of aString: ', aString);
+console.log('value of aNumber: ', aNumber);
+console.log('value of aBoolean: ', aBoolean);
+
+console.log('typeof anObject: ', typeof(anObject));
+console.log('typeof anArray: ', typeof(anArray));
+console.log('typeof aFunction: ', typeof(aFunction));
+console.log('typeof aString: ', typeof(aString));
+console.log('typeof aNumber: ', typeof(aNumber));
+console.log('typeof aBoolean: ', typeof(aBoolean));
