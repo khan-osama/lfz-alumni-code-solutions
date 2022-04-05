665a7eeb1aa3ba52591e1f57505713f10fbf6452
khan-osama <osamaa.khan61@gmail.com>
2022-04-05 11:17:07 -0700

    Moved query selector to the top of the js file

--- javascript-timers/javascript-timers/interval.js
+++ javascript-timers/javascript-timers/interval.js
@@ -1,7 +1,7 @@
+var h1 = document.querySelector('h1');
 var countdown = 3;
 
 var handle = setInterval(function() {
-  var h1 = document.querySelector('h1');
   h1.textContent = countdown;
   countdown--;
   if (h1.textContent === '0') {
