55ae03b57e06782d5643238e4ba1452c51b626b6
khan-osama <osamaa.khan61@gmail.com>
2022-04-07 11:17:20 -0700

    Logged module variables to console

---
+++ node-module-system/node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('exports: ', exports);
+console.log('typeof exports: ', typeof(exports));
+
+console.log('require: ', require);
+console.log('typeof require: ', typeof(require));
+
+console.log('module: ', module);
+console.log('typeof module: ', typeof(module));
+
+console.log('__filename: ', __filename);
+console.log('typeof __filename: ', typeof(__filename));
+
+console.log('__dirname: ', __dirname);
+console.log('typeof __dirname: ', typeof(__dirname));
