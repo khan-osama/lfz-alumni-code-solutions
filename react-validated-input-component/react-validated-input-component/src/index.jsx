89b526e3b1221d06c898db0128274807ffae0391
khan-osama <osamaa.khan61@gmail.com>
2022-04-28 16:05:55 -0700

    Created validated password via react

---
+++ react-validated-input-component/react-validated-input-component/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ValidatedInput from './validated-input'
+
+const element = <ValidatedInput />
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+)
