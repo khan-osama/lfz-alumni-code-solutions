b344f2b52161820c7f59f749e69d571ff14df17f
khan-osama <osamaa.khan61@gmail.com>
2022-04-28 21:08:55 -0700

    Created app drawer with react

---
+++ react-app-drawer-component/react-app-drawer-component/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppDrawer from './app-drawer'
+
+const element = <AppDrawer />
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+)
