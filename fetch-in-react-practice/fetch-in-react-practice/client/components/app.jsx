15c2cf1243d353bbbd7ed64ac1ea61488a129654
khan-osama <osamaa.khan61@gmail.com>
2022-05-03 14:22:21 -0700

    Created fetch requests to get, post and patch todos

---
+++ fetch-in-react-practice/fetch-in-react-practice/client/components/app.jsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import PageTitle from './page-title';
+import TodoList from './todo-list';
+import TodoForm from './todo-form';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.toggleCompleted = this.toggleCompleted.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllTodos();
+  }
+
+  getAllTodos() {
+    /**
+     * Use fetch to send a GET request to `/api/todos`.
+     * Then 😉, once the response JSON is received and parsed,
+     * update state with the received todos.
+     */
+    fetch('/api/todos')
+      .then(data => {
+        return data.json()
+      })
+      .then(todos => {
+        this.setState({
+          todos: todos
+        })
+      })
+  }
+
+  addTodo(newTodo) {
+    /**
+    * Use fetch to send a POST request to `/api/todos`.
+    * Then 😉, once the response JSON is received and parsed,
+    * add the created todo to the state array.
+    *
+    * TIP: Be sure to SERIALIZE the todo object in the body with JSON.stringify()
+    * and specify the "Content-Type" header as "application/json"
+    */
+   const options = {
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json'
+     },
+     body: JSON.stringify(newTodo)
+   }
+
+   fetch('/api/todos', options)
+    .then(data => {
+      return data.json()
+    })
+    .then(newTodo => {
+      this.setState({
+        todos: [...this.state.todos, newTodo]
+      })
+    })
+  }
+
+  toggleCompleted(todoId) {
+    /**
+     * Find the index of the todo with the matching todoId in the state array.
+     * Get its "isCompleted" status.
+     * Make a new object containing the opposite "isCompleted" status.
+     * Use fetch to send a PATCH request to `/api/todos/${todoId}`
+     * Then 😉, once the response JSON is received and parsed,
+     * replace the old todo in the state array.
+     *
+     * NOTE: "toggle" means to flip back and forth, so clicking a todo
+     * in the list should "toggle" its isCompleted status back and forth.
+     *
+     * TIP: Be sure to SERIALIZE the updates in the body with JSON.stringify()
+     * And specify the "Content-Type" header as "application/json"
+     */
+    const { todos } = this.state
+    todos.map((obj, index) => {
+      if (todoId === obj.todoId) {
+        let objStatus = obj.isCompleted
+        let completeObj = {
+          isCompleted: !objStatus
+        }
+
+        const options = {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(completeObj)
+        }
+
+        fetch(`/api/todos/${todoId}`, options)
+          .then(data => {
+            return data.json()
+          })
+          .then(updatedTodo => {
+            const updatedState = todos.slice()
+            updatedState[todoId - 1] = updatedTodo
+            this.setState({
+              todos: updatedState
+            })
+          })
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col pt-5">
+            <PageTitle text="Todo App"/>
+            <TodoForm onSubmit={this.addTodo}/>
+            <TodoList todos={this.state.todos} toggleCompleted={this.toggleCompleted}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
