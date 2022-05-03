ea626ab7c6aa974721d1f78f6644c279b16e9187
khan-osama <osamaa.khan61@gmail.com>
2022-05-03 14:27:50 -0700

    Fixed lastChar function

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,10 +1,5 @@
 /* exported lastChars */
 function lastChars(length, string) {
-  var lastString = [];
-  for (var i = 0; i < length; i++) {
-    if (string[i]) {
-      lastString.unshift(string[string.length - i - 1]);
-    }
-  }
-  return lastString.join('');
+  const lastChar = string.substring(string.length - length, string.length)
+  return lastChar
 }
