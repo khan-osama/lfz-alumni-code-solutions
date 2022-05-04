414b82f7cd183b58888cfde0478c64111b929c80
khan-osama <osamaa.khan61@gmail.com>
2022-05-04 12:06:56 -0700

    Passed data as a prop to component

--- react-accordion-component/react-accordion-component/src/accordion.jsx
+++ react-accordion-component/react-accordion-component/src/accordion.jsx
@@ -1,42 +1,4 @@
 import React from 'react'
-
-const data = [
-  {
-    title: `Hypertext Markup Language`,
-    content: `Hypertext Markup Language (HTML) is the standard markup language
-    for creating web pages and web applications. With Cascading Style Sheets (CSS)
-     and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.`
-  },
-  {
-    title: `Cascading Style Sheets`,
-    content: `Cascading Style Sheets (CSS) is a style sheet language used for describing
-    the presentation of a document written in a markup language like HTML. CSS is a cornerstone
-    technology of the worldwide web alongside HTML and JavaScript.`
-  },
-  {
-    title: `JavaScript`,
-    content: `JavaScript, often abbreviated as JS, is a high-level, interpreted programming
-    language that conforms to the ECMAscript specification. JavaScript has curly-bracket syntax,
-    dynamic typing, prototype-based object-orientation, and first-class functions.`
-  }
-]
-
-class AccordionData extends React.Component {
-  <div className='data'>
-  {data.map((object, index) => (
-    <div className='header' onClick={() => {this.props.handleClick(index)}}>
-      <h1>
-        {object.title}
-      </h1>
-      <div className={`${(this.props.topicSelected === index) &&
-      this.props.accordionOpen ? 'content-show' : 'hidden'}`}>
-        <p>{object.content}</p>
-      </div>
-    </div>
-  ))}
-</div>
-}
-
 class Accordion extends React.Component {
   constructor(props) {
     super(props)
@@ -48,9 +10,22 @@ class Accordion extends React.Component {
   }
 
   render() {
+    const { data } = this.props
     return (
       <div className='container'>
-        <AccordionData topicSelected={this.state.topicSelected} accordionOpen={this.state.accordionOpen} handleClick={this.handleClick} />
+          <div className='data'>
+            {data.map((object, index) => (
+              <div className='header' onClick={() => {this.handleClick(index)}}>
+                <h1>
+                  {object.title}
+                </h1>
+                <div className={`${(this.state.topicSelected === index) &&
+                this.state.accordionOpen ? 'content-show' : 'hidden'}`}>
+                  <p>{object.content}</p>
+                </div>
+              </div>
+            ))}
+          </div>
       </div>
     )
   }
--- react-accordion-component/react-accordion-component/src/index.jsx
+++ react-accordion-component/react-accordion-component/src/index.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Accordion from './accordion'
 
-const element = <Accordion />
+const data = [
+  {
+    title: `Hypertext Markup Language`,
+    content: `Hypertext Markup Language (HTML) is the standard markup language
+    for creating web pages and web applications. With Cascading Style Sheets (CSS)
+     and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.`
+  },
+  {
+    title: `Cascading Style Sheets`,
+    content: `Cascading Style Sheets (CSS) is a style sheet language used for describing
+    the presentation of a document written in a markup language like HTML. CSS is a cornerstone
+    technology of the worldwide web alongside HTML and JavaScript.`
+  },
+  {
+    title: `JavaScript`,
+    content: `JavaScript, often abbreviated as JS, is a high-level, interpreted programming
+    language that conforms to the ECMAscript specification. JavaScript has curly-bracket syntax,
+    dynamic typing, prototype-based object-orientation, and first-class functions.`
+  }
+]
+
+const element = <Accordion data={data} />
 
 ReactDOM.render(
   element,
