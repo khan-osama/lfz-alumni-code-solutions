a91156f1319e42f0a4d421e89322b5394eb9b632
khan-osama <osamaa.khan61@gmail.com>
2022-06-23 17:47:37 -0700

    Refactored code to use react hooks

--- react-toggle-switch-component/react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,28 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-class ToggleSwitch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isToggled: false }
-    this.handleClick = this.handleClick.bind(this)
+// class ToggleSwitch extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { isToggled: false }
+//     this.handleClick = this.handleClick.bind(this)
+//   }
+
+//   render() {
+//     return (
+//       <div className='container'>
+//         <label className='switch'>
+//           <input onClick={this.handleClick} type="checkbox"></input>
+//           <span className='slider'></span>
+//         </label>
+//       </div>
+//     )
+//   }
+
+//   handleClick() {
+//     this.setState({
+//       isToggled: !this.state.isToggled
+//     })
+//   }
+// }
+
+// export default ToggleSwitch
+
+
+function ToggleSwitch() {
+  const [isToggled, setToggle] = useState(false)
+
+  function handleClick() {
+    setToggle(!isToggled)
   }
 
-  render() {
-    return (
-      <div className='container'>
-        <label className='switch'>
-          <input onClick={this.handleClick} type="checkbox"></input>
-          <span className='slider'></span>
-        </label>
-      </div>
-    )
-  }
-
-  handleClick() {
-    this.setState({
-      isToggled: !this.state.isToggled
-    })
-  }
+  return (
+    <div className='container'>
+      <label className='switch'>
+        <input onClick={handleClick} type="checkbox"></input>
+        <span className='slider'></span>
+      </label>
+  </div>
+  )
 }
 
 export default ToggleSwitch
